ef8ab01020b02f7a25c67bef293495a258d541bf
joneshf <jones3.hardy@gmail.com>
2018-09-29 19:29:40 -0700

    Case explicitly
    Maybe this is a bit clearer?
    Seems like we'd be better off if we had the two values straight up,
    rather than using an `Array Number`.
    Probably once more is in PS, we can type things a bit better.

--- GUI/src/Render.purs
+++ GUI/src/Render.purs
@@ -14,7 +14,7 @@ import Data.FoldableWithIndex (forWithIndex_)
 import Data.Function.Uncurried (Fn1, mkFn1)
 import Data.Int (toNumber)
 import Data.Map (lookup)
-import Data.Maybe (maybe)
+import Data.Maybe (Maybe(..), maybe)
 import Data.Newtype (wrap)
 import Data.String (split, toCodePointArray)
 import Data.Traversable (for)
@@ -50,64 +50,62 @@ calcFontPoint text tilt offsety offsetx { x, y } =
   point0 = x * text.width + offsetx
   point1 = y * text.height + offsety
 
-
 deg2rad :: Fn1 Number Number
 deg2rad = mkFn1 \deg ->
   deg * Math.pi / 180.0
 
-drawtext :: EffectFn4 Context2D Text String Boolean Unit
-drawtext = mkEffectFn4 \ctx text color flip ->
-  for_
-    ({translateX: _, translateY: _} <$> text.pos !! 0 <*> text.pos !! 1)
-    \pos -> do
-      save ctx
-      translate ctx pos
-      angle <- if "mirrored" `elem` text.attr
-        then do
-          scale ctx { scaleX: -1.0, scaleY: 1.0 }
-          pure text.angle
-        else pure (-text.angle)
-      let tilt = if "italic" `elem` text.attr then 0.125 else 0.0
-          interline = (text.height * 1.5 + text.thickness) / 2.0
-          txt = split (wrap "\n") text.text
-      rotate ctx (deg2rad angle)
-      setFillStyle ctx color
-      setStrokeStyle ctx color
-      setLineCap ctx Round
-      setLineWidth ctx text.thickness
-      forWithIndex_ txt \i txti -> do
-        let lineWidth = maybe 0.0 _.offsetx (last widths)
-            offsety =
-              toNumber (i * 2 - length txt + 1) * interline + text.height / 2.0
-            widths = mkWidths text txti
-            justify = case text.horiz_justify of
-              -- Justify left, do nothing
-              -1 -> 0.0
-              -- Justify center
-              0 -> lineWidth / -2.0
-              -- Justify right
-              1 -> -lineWidth
-              -- We have to handle the 2^54 - 3 other cases.
-              _ -> 0.0
-        for_ widths \{ l, offsetx, width } -> do
-          let calc = calcFontPoint text tilt offsety (justify + width)
-          for_ l \line -> do
-            -- Drawing each segment separately instead of
-            -- polyline because round line caps don't work in joints
-            beginPath ctx
-            uncurry (moveTo ctx) (calc $ head line)
-            traverse_ (uncurry (lineTo ctx) <<< calc) (tail line)
-            stroke ctx
-      restore ctx
+drawtext :: forall a. EffectFn4 Context2D Text String a Unit
+drawtext = mkEffectFn4 \ctx text color _ -> case text.pos !! 0, text.pos !! 1 of
+  Just translateX, Just translateY -> do
+    save ctx
+    translate ctx { translateX, translateY }
+    angle <- if "mirrored" `elem` text.attr
+      then do
+        scale ctx { scaleX: -1.0, scaleY: 1.0 }
+        pure text.angle
+      else pure (-text.angle)
+    let tilt = if "italic" `elem` text.attr then 0.125 else 0.0
+        interline = (text.height * 1.5 + text.thickness) / 2.0
+        txt = split (wrap "\n") text.text
+    rotate ctx (deg2rad angle)
+    setFillStyle ctx color
+    setStrokeStyle ctx color
+    setLineCap ctx Round
+    setLineWidth ctx text.thickness
+    forWithIndex_ txt \i txti -> do
+      let lineWidth = maybe 0.0 _.offsetx (last characters)
+          offsety =
+            toNumber (i * 2 - length txt + 1) * interline + text.height / 2.0
+          characters = mkCharacters text txti
+          justify = case text.horiz_justify of
+            -- Justify left, do nothing
+            -1 -> 0.0
+            -- Justify center
+            0 -> lineWidth / -2.0
+            -- Justify right
+            1 -> -lineWidth
+            -- We have to handle the 2^54 - 3 other cases.
+            _ -> 0.0
+      for_ characters \{ l, offsetx, width } -> do
+        let calc = calcFontPoint text tilt offsety (justify + width)
+        for_ l \line -> do
+          -- Drawing each segment separately instead of
+          -- polyline because round line caps don't work in joints
+          beginPath ctx
+          uncurry (moveTo ctx) (calc $ head line)
+          traverse_ (uncurry (lineTo ctx) <<< calc) (tail line)
+          stroke ctx
+    restore ctx
+  _, _ -> mempty
 
-type Width =
+type Character =
   { l :: Array (NonEmptyArray Point)
   , offsetx :: Number
   , width :: Number
   }
 
-mkWidths :: forall r. { width :: Number | r } -> String -> Array Width
-mkWidths text str =
+mkCharacters :: forall r. { width :: Number | r } -> String -> Array Character
+mkCharacters text str =
   fold $ flip evalStateT 0.0 $ for (toCodePointArray str) \c -> do
     { l, w } <- lift (lookup c pcbFont.font_data)
     width <- get
